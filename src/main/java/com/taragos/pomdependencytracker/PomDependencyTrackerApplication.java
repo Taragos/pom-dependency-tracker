e5583ce85c963f5c80e686c4f280967e3b18a0f4
Taragos <32304118+Taragos@users.noreply.github.com>
2022-03-20 12:41:32 +0100

    Basic Setup for #1

---
+++ src/main/java/com/taragos/pomdependencytracker/PomDependencyTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.taragos.pomdependencytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomDependencyTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomDependencyTrackerApplication.class, args);
+    }
+
+}
+
