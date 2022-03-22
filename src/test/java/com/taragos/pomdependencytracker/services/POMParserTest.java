dc6706ab93397fa0edc2ddfa844e4687ab64719e
Taragos <32304118+Taragos@users.noreply.github.com>
2022-03-22 05:52:42 +0100

    #7 - Add POMParserTest and fix wrong parsing in POMParser

--- src/test/java/com/taragos/pomdependencytracker/services/POMParserTest.java
+++ src/test/java/com/taragos/pomdependencytracker/services/POMParserTest.java
@@ -4,41 +4,50 @@ import com.taragos.pomdependencytracker.domain.ArtifactEntity;
 import com.taragos.pomdependencytracker.domain.DependencyRelationship;
 import com.taragos.pomdependencytracker.exceptions.FieldParseException;
 import com.taragos.pomdependencytracker.infrastructure.services.DependencyTreeParser;
+import com.taragos.pomdependencytracker.infrastructure.services.POMParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 import java.util.Optional;
 
-public class DependencyTreeParserTest {
+public class POMParserTest {
 
-    final DependencyTreeParser dependencyTreeParser = new DependencyTreeParser();
-    static String dependencyTree;
+    final POMParser pomParser = new POMParser();
+    static String pom;
 
     @BeforeAll
     static void init() throws IOException {
         final ClassLoader classLoader = DependencyTreeParserTest.class.getClassLoader();
-        final InputStream inputStream = Objects.requireNonNull(classLoader.getResourceAsStream("dependencyTree.txt"));
-        dependencyTree = new String(inputStream.readAllBytes());
+        final InputStream inputStream = Objects.requireNonNull(classLoader.getResourceAsStream("pom.xml"));
+        pom = new String(inputStream.readAllBytes());
     }
 
     @Test
     void testParse() {
         try {
-            final ArtifactEntity parse = dependencyTreeParser.parse(dependencyTree);
+            final ArtifactEntity parse = pomParser.parse(pom);
             Assertions.assertEquals("com.taragos", parse.getGroupId());
             Assertions.assertEquals("pom-dependency-tracker", parse.getArtifactId());
             Assertions.assertEquals("0.0.1-SNAPSHOT", parse.getVersion());
+
+            final ArtifactEntity parent = parse.getParent();
+
+            Assertions.assertNotNull(parent);
+            Assertions.assertEquals("org.springframework.boot", parent.getGroupId());
+            Assertions.assertEquals("spring-boot-starter-parent", parent.getArtifactId());
+            Assertions.assertEquals("2.6.4", parent.getVersion());
 
             Assertions.assertEquals(6, parse.getDependencies().size());
 
             final Optional<DependencyRelationship> optionalDep = parse
                     .getDependencies()
                     .stream()
-                    .filter(d-> Objects.equals(d.getDependency().getGAV(), "org.springframework.boot:spring-boot-starter-data-neo4j:2.6.4"))
+                    .filter(d-> Objects.equals(d.getDependency().getGAV(), "org.springframework.boot:spring-boot-starter-data-neo4j:null"))
                     .findFirst();
 
             Assertions.assertTrue(optionalDep.isPresent());
@@ -47,10 +56,10 @@ public class DependencyTreeParserTest {
 
             Assertions.assertEquals("org.springframework.boot", dep.getDependency().getGroupId());
             Assertions.assertEquals("spring-boot-starter-data-neo4j", dep.getDependency().getArtifactId());
-            Assertions.assertEquals("2.6.4", dep.getDependency().getVersion());
-            Assertions.assertEquals("jar", dep.getType());
-            Assertions.assertEquals("compile", dep.getScope());
-            Assertions.assertEquals(2, dep.getDependency().getDependencies().size());
+            Assertions.assertNull(dep.getDependency().getVersion());
+            Assertions.assertNull(dep.getType());
+            Assertions.assertNull(dep.getScope());
+            Assertions.assertEquals(0, dep.getDependency().getDependencies().size());
 
         } catch (FieldParseException e) {
             Assertions.fail(e.getMessage());
