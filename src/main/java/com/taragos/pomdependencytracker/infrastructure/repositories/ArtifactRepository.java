9b8841b0b8061997133593fc9b209c4c5d1dcbe1
Taragos <32304118+Taragos@users.noreply.github.com>
2022-05-16 11:49:32 +0200

    #10 - implement a basic web ui for searching, current search allows artifacts

--- src/main/java/com/taragos/pomdependencytracker/infrastructure/repositories/ArtifactRepository.java
+++ src/main/java/com/taragos/pomdependencytracker/infrastructure/repositories/ArtifactRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ArtifactRepository extends Neo4jRepository<ArtifactEntity, String> {
     @Query("MATCH(n: Artifact {id: $artifactId})-[r:DEPENDENCY]-(a:Artifact) WHERE ID(r) = $dependencyRelationshipId DELETE r")
     void detachDependencyFromArtifact(@Param("artifactId") String artifactId, @Param("dependencyRelationshipId") Long dependencyRelationshipId);
+
+    @Query("MATCH (n: Artifact) WHERE n.artifactId =~ $artifactId AND n.groupId =~ $groupId AND n.version =~ $version RETURN n")
+    List<ArtifactEntity> findArtifactsByRegex(@Param("artifactId") String artifactId, @Param("groupId") String groupId, @Param("version") String version);
 }
